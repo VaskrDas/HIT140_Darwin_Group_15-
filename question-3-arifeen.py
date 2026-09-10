3bf08e48ecc20bf0d6bd536124230f0b5eec9f4f
Arifeenh-net <arifeenh18@gmail.com>
2026-09-10 14:26:07 +0930

    Finalized - Arifeen

--- question-3-arifeen.py
+++ question-3-arifeen.py
@@ -3,7 +3,7 @@ import math
 import scipy.stats as stats
 
 # load the data
-df = pd.read_csv("players.csv")
+df = pd.read_csv("question-3-dataset-arifeen.csv")
 
 # get only defenders and midfielders
 filtered_data = df[(df["Pos"] == "DF") | (df["Pos"] == "MF")]
